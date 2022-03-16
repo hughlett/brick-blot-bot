04c3b81c61212d19ef153e16cb0d445fe9960402
arhughle <arhughle@ncsu.edu>
2022-03-16 14:24:16 -0400

    Fixed path

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3
 COPY . /brick_blot_bot
 WORKDIR /brick_blot_bot
 RUN pip install -r requirements.txt
+RUN ["chmod", "+x", "./wait-for-it.sh"]
 CMD ["./wait-for-it.sh" , "http://chrome:4444" , "--" , "python3", "brick_blot_bot.py"]
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PROJECT_PATH=`dirname $0`
 COMPOSE_PATH=${PROJECT_PATH}/docker-compose.yml
 
 cd ${PROJECT_PATH} && git pull
-docker-compose -f ${COMPOSE_PATH}/docker-compose.yml up --build --abort-on-container-exit && docker-compose -f ${COMPOSE_PATH} down
+docker-compose -f ${COMPOSE_PATH} up --build --abort-on-container-exit && docker-compose -f ${COMPOSE_PATH} down
