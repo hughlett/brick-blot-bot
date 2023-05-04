b0a7ff819e0ce55bbd148fb3b3b79a119ce8d540
Drew Hughlett <drewrh04@gmail.com>
2023-05-04 23:48:31 +0000

    🔥 Remove old deployment

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROJECT_PATH=`dirname $0`
-COMPOSE_PATH=${PROJECT_PATH}/docker-compose.yml
-
-cd ${PROJECT_PATH} && git pull
-docker-compose -f ${COMPOSE_PATH} up --build --abort-on-container-exit && docker-compose -f ${COMPOSE_PATH} down
