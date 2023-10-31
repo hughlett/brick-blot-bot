0628fe95de53ebbcdf2d615dc07583a0de86ad08
Drew Hughlett <drewrh04@gmail.com>
2023-10-30 20:21:43 -0400

    🚚 Restructure project

---
+++ brick-blot-bot/__main__.py
@@ -0,0 +1,19 @@
+import tweepy
+from os import environ
+from datetime import date, timedelta
+from dotenv import load_dotenv
+
+from tweets import tweet_reports
+
+
+load_dotenv()
+# create_table()
+
+client = tweepy.Client(
+    consumer_key=environ.get("CONSUMER_KEY"),
+    consumer_secret=environ.get("CONSUMER_KEY_SECRET"),
+    access_token=environ.get("ACCESS_TOKEN"),
+    access_token_secret=environ.get("ACCESS_TOKEN_SECRET"),
+)
+
+tweet_reports(date.today() - timedelta(30), date.today(), client)
